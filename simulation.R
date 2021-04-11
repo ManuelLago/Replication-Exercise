504da619342fbd252af2c5d98bb186bb02c9483a
jingjing619 <77918240+jingjing619@users.noreply.github.com>
2021-04-12 02:44:30 +0800

    Add files via upload

--- simulation.R
+++ simulation.R
@@ -1,14 +1,50 @@
 install.packages("DRDID")
 library(DRDID)
-data("sim_rc")
-covX = as.matrix(sim_rc[,5:8])
-reg_did_rc(y = sim_rc$y, post = sim_rc$post, D = sim_rc$d,covariates= covX)
-twfe_did_rc(y = sim_rc$y, post = sim_rc$post, D = sim_rc$d,covariates= covX)
-ipw_did_rc(y = sim_rc$y, post = sim_rc$post, D = sim_rc$d,covariates= covX)
-std_ipw_did_rc(y = sim_rc$y, post = sim_rc$post, D = sim_rc$d,covariates= covX)
-drdid_rc1(y = sim_rc$y, post = sim_rc$post, D = sim_rc$d,
-          covariates= covX)
-drdid_imp_rc1(y = sim_rc$y, post = sim_rc$post, D = sim_rc$d,covariates= covX)
-drdid_rc2(y = sim_rc$y, post = sim_rc$post, D = sim_rc$d,
-          covariates= covX)
-drdid_imp_rc2(y = sim_rc$y, post = sim_rc$post, D = sim_rc$d,covariates= covX)
+data("nsw_long")
+# Form the Lalonde sample with CPS comparison group
+eval_lalonde_cps <- subset(nsw_long, nsw_long$treated == 0 | nsw_long$sample == 2)
+# Implement OR DID with panel data
+ordid(yname="re", tname = "year", idname = "id", dname = "experimental",
+      xformla= ~ age+ educ+ black+ married+ nodegree+ hisp+ re74,
+      data = eval_lalonde_cps, panel = TRUE)
+
+# Form the Lalonde sample with CPS comparison group
+eval_lalonde_cps <- subset(nsw, nsw$treated == 0 | nsw$sample == 2)
+# Select some covariates
+covX = as.matrix(cbind(eval_lalonde_cps$age, eval_lalonde_cps$educ,
+                       eval_lalonde_cps$black, eval_lalonde_cps$married,
+                       eval_lalonde_cps$nodegree, eval_lalonde_cps$hisp,
+                       eval_lalonde_cps$re74))
+# Implement TWFE DID with panel data
+twfe_did_panel(y1 = eval_lalonde_cps$re78, y0 = eval_lalonde_cps$re75,
+               D = eval_lalonde_cps$experimental,
+               covariates = covX)
+
+# Form the Lalonde sample with CPS comparison group
+eval_lalonde_cps <- subset(nsw_long, nsw_long$treated == 0 | nsw_long$sample == 2)
+
+# Implement IPW DID with panel data (normalized weights)
+ipwdid(yname="re", tname = "year", idname = "id", dname = "experimental",
+       xformla= ~ age+ educ+ black+ married+ nodegree+ hisp+ re74,
+       data = eval_lalonde_cps, panel = TRUE)
+
+# Form the Lalonde sample with CPS comparison group
+eval_lalonde_cps <- subset(nsw_long, nsw_long$treated == 0 | nsw_long$sample == 2)
+# -----------------------------------------------
+# Implement improved DR locally efficient DID with panel data
+drdid(yname="re", tname = "year", idname = "id", dname = "experimental",
+      xformla= ~ age+ educ+ black+ married+ nodegree+ hisp+ re74,
+      data = eval_lalonde_cps, panel = TRUE)
+
+# Form the Lalonde sample with CPS comparison group (data in wide format)
+eval_lalonde_cps <- subset(nsw, nsw$treated == 0 | nsw$sample == 2)
+# Select some covariates
+covX = as.matrix(cbind(eval_lalonde_cps$age, eval_lalonde_cps$educ,
+                       eval_lalonde_cps$black, eval_lalonde_cps$married,
+                       eval_lalonde_cps$nodegree, eval_lalonde_cps$hisp,
+                       eval_lalonde_cps$re74))
+
+# Implement traditional DR locally efficient DID with panel data
+drdid_panel(y1 = eval_lalonde_cps$re78, y0 = eval_lalonde_cps$re75,
+            D = eval_lalonde_cps$experimental,
+            covariates = covX)
